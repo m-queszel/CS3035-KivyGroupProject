91d026956cc71847b9dd2583fa48a4212039c242
Queszel <QueszelEdu@gmail.com>
2025-10-05 13:46:47 -0300

    added rock/paper/scissors buttons and introduced basic event handling

--- Kivy-RPS/main.py
+++ Kivy-RPS/main.py
@@ -1,15 +1,24 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 
-class rpsGame(Widget):
-    pass
+class GameGridLayout(Widget):
+    rock = ObjectProperty(None)
+    paper = ObjectProperty(None)
+    scissors = ObjectProperty(None)
+    scoreStatus = ObjectProperty(None)
+
+    def press(self, instance):
+        self.scoreStatus.text = "changed!"
 
 
 class RPSApp(App):
     def build(self):
-        return rpsGame()
+        return GameGridLayout()
 
 
 if __name__ == '__main__':
