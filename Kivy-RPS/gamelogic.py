402a1d146c6df6548427fae645ff096b68c93cdb
Queszel <QueszelEdu@gmail.com>
2025-10-07 13:23:00 -0300

    Fixed layout and added score tracking

--- Kivy-RPS/gamelogic.py
+++ Kivy-RPS/gamelogic.py
@@ -1,27 +1,36 @@
 # Returns 1 if Player wins, returns 0 if computer wins, returns 2 if tie
-def checkWin(mainPlayerRoll, computerRoll):
+
+def checkWin(mainPlayerRoll, computerRoll, player, computer):
     if mainPlayerRoll == "paper":
         if computerRoll == "paper":
             return 2
         if computerRoll == "rock":
+            player.increaseScore()
             return 1
         if computerRoll == "scissors":
+            computer.increaseScore()
             return 0
 
     if mainPlayerRoll == "rock":
         if computerRoll == "rock":
             return 2
         if computerRoll == "paper":
+            computer.increaseScore()
             return 0
+
         if computerRoll == "scissors":
+            player.increaseScore()
             return 1
 
     if mainPlayerRoll == "scissors":
         if computerRoll == "scissors":
             return 2
         if computerRoll == "rock":
+            computer.increaseScore()
             return 0
+
         if computerRoll == "paper":
+            player.increaseScore()
             return 1
 
 
@@ -32,3 +41,7 @@ def displayWinner(num):
         return "You win!"
     if num == 2:
         return "Tie!"
+
+
+def displayScore(humanScore, computerScore):
+    return "Your score: " + str(humanScore) + " | Computer score: " + str(computerScore)
--- Kivy-RPS/main.py
+++ Kivy-RPS/main.py
@@ -2,28 +2,42 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from computer import playTurn
-from gamelogic import checkWin, displayWinner
+from gamelogic import checkWin, displayWinner, displayScore
+
+
+class Player():
+    def __init__(self):
+        self.score = 0
+
+    def increaseScore(self):
+        self.score = self.score + 1
 
 
 class GameLayout(Widget):
 
+    player = Player()
+    computer = Player()
+
     def pressRock(self, instance):
         compRoll = playTurn()
         self.computerRoll.text = "You played rock and the computer rolled " + compRoll
-        result = checkWin("rock", compRoll)
-        self.scoreStatus.text = displayWinner(result)
+        result = checkWin("rock", compRoll, self.player, self.computer)
+        self.score.text = displayScore(self.player.score, self.computer.score)
+        self.winLabel.text = displayWinner(result)
 
     def pressPaper(self, instance):
         compRoll = playTurn()
         self.computerRoll.text = "You played paper and the computer rolled " + compRoll
-        result = checkWin("paper", compRoll)
-        self.scoreStatus.text = displayWinner(result)
+        result = checkWin("paper", compRoll, self.player, self.computer)
+        self.score.text = displayScore(self.player.score, self.computer.score)
+        self.winLabel.text = displayWinner(result)
 
     def pressScissors(self, instance):
         compRoll = playTurn()
         self.computerRoll.text = "You played scissors and the computer rolled " + compRoll
-        result = checkWin("scissors", compRoll)
-        self.scoreStatus.text = displayWinner(result)
+        result = checkWin("scissors", compRoll, self.player, self.computer)
+        self.score.text = displayScore(self.player.score, self.computer.score)
+        self.winLabel.text = displayWinner(result)
 
 
 class RPSApp(App):
